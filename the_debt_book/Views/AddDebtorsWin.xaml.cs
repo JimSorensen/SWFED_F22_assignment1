ee73b36318065be6e5db7395365ac44477306f25
JimSorensen <56436477+JimSorensen@users.noreply.github.com>
2022-03-02 10:44:00 +0100

    Nye vinduer

---
+++ the_debt_book/Views/AddDebtorsWin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace the_debt_book.Views
+{
+	/// <summary>
+	/// Interaction logic for AddDebtorsWin.xaml
+	/// </summary>
+	public partial class AddDebtorsWin : Window
+	{
+		public AddDebtorsWin()
+		{
+			InitializeComponent();
+		}
+	}
+}
