888360974b652127d2ef468c9427142f63fd3193
alh <alh@CCHJUL2018>
2022-03-03 10:24:29 +0100

    first commit

--- the_debt_book/Models/DebtorsModel.cs
+++ the_debt_book/Models/DebtorsModel.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace the_debt_book.Models
 {
-	internal class Debtors
-	{
-	}
+    public class DebtorsModel
+    {
+
+        public String FullName { get; set; }
+        public List<DebtsModel> Debts { get; set; }
+    }
 }
