30e9006f3a0bc7087021f4a4164be436e94d2e8d
JimSorensen <56436477+JimSorensen@users.noreply.github.com>
2022-03-08 09:19:21 +0100

    Forsøger at lave en Model

---
+++ The_Debt_Book/ViewModels/DepterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Debt_Book.Models;
+
+namespace The_Debt_Book.ViewModels
+{
+	internal class DepterViewModel
+	{
+		private Depter? itemSelected;
+
+		public ObservableCollection<Depter> Depters { get; set; }
+
+		public DepterViewModel()
+		{
+			this.Depters = new ObservableCollection<Depter>();
+			PopulateStaticData();
+		}
+	}
+}
